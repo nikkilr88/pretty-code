3287a45070b49e927cdcd6ae6d255f4bbe5e60c3
Nicole <nicole.sattler@live.com>
2018-05-18 20:02:45 -0500

    Change render to sendFile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.sendFile('index.html')
 });
 
 app.listen(3000, () => console.log('Server started on port 3000...'));
